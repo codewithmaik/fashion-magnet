696692be7868e3636dd0bf95156ec7b67dd151a0
codewithmaik <maik.cawelius@startmail.com>
2023-12-01 17:20:28 +0000

    Create database model and load fixtures
    Add models for categories and products, migrate changes, register models in the admin file and load data from fixtures.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    ''' Model for a category '''
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    
+    def __str__(self):
+        ''' Returns the categories name as a string '''
+        return self.name
+
+    
+    def get_friendly_name(self):
+        ''' Returns the user-friendly name as a string '''
+        return self.friendly_name
+
+
+
+class Product(models.Model):
+    ''' Model for a product '''
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+def __str__(self):
+    ''' Returns the products name as a string '''
+    return self.name
+
+
+
