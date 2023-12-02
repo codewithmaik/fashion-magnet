1935d98971e00d5ce0416c250aadbf87380cc2e3
codewithmaik <maik.cawelius@startmail.com>
2023-12-02 15:38:31 +0000

    Customize admin panel
    Add classes for customizing list view in admin panel for categories and products.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    ''' Class to modify the categories in the admin panel '''
+
+    # Category fields to display in the admin panel
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    ''' Class to modify the products in the admin panel '''
+    
+    # Product fields to display in the admin panel
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image'
+    )
+        
+    # Sorting the products in the admin panel by SKU
+    ordering = ('sku',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
