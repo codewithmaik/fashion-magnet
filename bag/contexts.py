b26140ed6d9162f4e47a1e24095d7220892340a5
codewithmaik <maik.cawelius@startmail.com>
2023-12-19 15:57:35 +0000

    Update context processor
    Add current shopping bag information to be able to access it all over the application.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,30 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+
+    # Set bag variable equal to current value in the session if it already exists, setting it equal to an empty dictionary otherwise
+    bag = request.session.get('bag', {})
+
+    # Get product for every item in bag, calculate the total price and increase the product count
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total = quantity * product.price
+        product_count += quantity
+
+        # Add object to list of items to access it all over the application
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
